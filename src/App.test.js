5248f823ebfa0d2e41d3877a5ff9d33bf84cec6b
Ajesh Sen Thapa <aj3sshh@gmail.com>
2022-07-04 20:58:40 +0545

    404 page test case added

--- src/App.test.js
+++ src/App.test.js
@@ -24,3 +24,13 @@ test("test if home page is redirected to login page", async () => {
   const loginButton = await screen.findByTestId('test_login')
   expect(loginButton).toBeInTheDocument();
 });
+
+test("test 404 page", async () => {
+  const a = render(
+    <MemoryRouter initialEntries={["/404-page"]}>
+      <App/>
+    </MemoryRouter>
+  );
+  const element = await screen.findByTestId('test_404')
+  expect(element).toBeInTheDocument();
+});
--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,5 +1,5 @@
-function NotFoundPage(){
-    return <div>NotFoundPage</div>
+const NotFoundPage = () => {
+    return <h1 data-testid="test_404">404 Page Not Found</h1>
 }
 
 export default NotFoundPage
